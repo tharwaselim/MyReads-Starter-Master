aefd70316a0b5aa2ede777ecf0dead9da3c67e35
Tharwa Selim <79284858+tharwaselim@users.noreply.github.com>
2021-06-24 13:51:14 +0200

    Update Shelf.js

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,51 +1,22 @@
-import React from 'react'
+import React from "react";
+import Book from "./Book";
 
-class Shelf extends React.Component {
+const Shelf = ({ books, Title, onChangeBookShelf }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{Title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.length > 0 &&
+            books.map((book) => (
+              <li key={book.id}>
+                <Book book={book} onChangeBookShelf={onChangeBookShelf} />
+              </li>
+            ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
 
-    render(){
-        const shelfBooks=this.props.books;
-        return (
-            
-                <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                      {shelfBooks.map(book=>(
-                          <li key={book.id}>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" style={{ 
-                                  width: 128, 
-                                  height: 193, 
-                                  backgroundImage:`url(${book.imageLinks.thumbnail})`
-                                   }}></div>
-                              <div className="book-shelf-changer">
-                                <select 
-                                value={book.shelf} 
-                                onChange={e=>
-                                this.props.changeShelf(book,e.target.value)
-                                }>
-                                  <option value="move" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                          </div>
-                        </li>
-                      ))}
-                  </ol>
-                </div>
-              </div>
-              
-
-              
-        )
-    }
-}
-
-    export  default Shelf;
+export default Shelf;
