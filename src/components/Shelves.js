dfe0a8eb66a112d96b4a59a09036153bbec10863
Tharwa Selim <79284858+tharwaselim@users.noreply.github.com>
2021-06-24 13:52:04 +0200

    Update Shelves.js

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -1,45 +1,62 @@
-import React from 'react';
-import Shelf from './Shelf';
+import React, { Component } from "react";
+import Shelf from "./Shelf";
+import { Link } from "react-router-dom";
 
-class Shelves extends React.Component {
+class Shelves extends Component {
+  getShelves = (books) => {
+    const currentlyReading = books.filter(
+      (book) => book.shelf === "currentlyReading"
+    );
+    const wantToRead = books.filter(
+      (book) => book.shelf === "wantToRead");
+    const read = books.filter(
+      (book) => book.shelf === "read");
+    return [
+      {
+        id: 1,
+        title: "Currently Reading",
+        books: currentlyReading,
+      },
+      {
+        id: 2,
+        title: "Want To Read",
+        books: wantToRead,
+      },
+      {
+        id: 3,
+        title: "Read",
+        books: read,
+      },
+    ];
+  };
 
-    render() {
-        const allBooks = this.props.allBooks;
-        
-        
+  render() {
+    const shelves = this.getShelves(this.props.books);
+    const { onChangeShelf } = this.props;
 
-        console.log("im hre",allBooks)
-
-
-
-        const currentlyReading=allBooks.filter(book=>book.shelf==="currentlyReading");
-        const wantToRead=allBooks.filter(book=>book.shelf==="wantToRead");
-        const read=allBooks.filter(book=>book.shelf==="read");
-        
-        return (
-            <div className="list-books-content">
-                <div>
-                {/*//shelf*/}
-                <Shelf 
-                books={currentlyReading} 
-                title={"Currently Reading"} 
-                changeShelf={this.props.changeShelf}/>
-                
-                {/*//shelf*/}
-                <Shelf 
-                books={wantToRead} 
-                title={"Want To Read"} 
-                changeShelf={this.props.changeShelf}/>
-
-                {/*//shelf*/}
-                <Shelf 
-                books={read} 
-                title={"Read"} 
-                changeShelf={this.props.changeShelf}/>
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          {shelves.length > 0 &&
+            shelves.map((shelf) => (
+              <Shelf
+                key={shelf.id}
+                books={shelf.books}
+                shelfTitle={shelf.title}
+                onChangeBookShelf={onChangeShelf}
+              />
+            ))}
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Shelves;
+
